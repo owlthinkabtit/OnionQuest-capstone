1f581fb2a80e1ed6e6c323f9737a9a468edc3b61
Meek Meek <haggins.jameka@gmail.com>
2026-03-30 09:49:15 -0400

    Seems a lot of things I can reuse, like the userSchema.

---
+++ models/User.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String, 
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String, 
+    required: true
+  }
+}, { timestamps: true});
+
+userSchema,pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const saltRounds = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, saltRounds);
+    next();
+  } catch(error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
