00103893f8bc4c8c013523177d348258d01d46a0
Meek Meek <haggins.jameka@gmail.com>
2026-03-30 15:05:57 -0400

    Backend complete...with Auth and CRUD, started on the READ.me as well

--- backend/routes/questRoutes.js
+++ backend/routes/questRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createQuest, getCampaignQuests } from "../controllers/questController.js";
+import { createQuest, getCampaignQuests, updateQuest, deleteQuest } from "../controllers/questController.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router({ mergeParams: true });
@@ -9,5 +9,7 @@ router.use(protect);
 router.route('/')
   .post(createQuest)
   .get(getCampaignQuests);
-
+router.route('/:id')
+  .put(updateQuest)
+  .delete(deleteQuest);
 export default router;
