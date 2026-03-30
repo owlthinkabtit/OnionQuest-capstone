90a6801efb2d55c6aaac7cf9cfe487cfdad6b75b
Meek Meek <haggins.jameka@gmail.com>
2026-03-30 10:39:03 -0400

    Forgot that I had to structure this between frontend and backend. Switched around the folder structure, and started working on the controllers and routes.

---
+++ backend/controllers/authController.js
@@ -0,0 +1,42 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '1d'
+  });
+};
+
+export const register = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.create({ username, password });
+    const token = createToken(user._id);
+
+    res.status(201).json({ user: { id: user._id, username: user.username }, token });
+  } catch(err) {
+    res.status(400).json({ error: 'That Character name is already taken!' });
+  }
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ error: 'Character not found!' });
+    }
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ error: 'Invalid credentials!' });
+    }
+
+    const token = createToken(user._id);
+    res.status(200).json({ user: { id: user._id, username: user.username }, token });
+  } catch(err) {
+    res.status(500).json({ error: 'The Gatekeeper is having trouble. Try again!' });
+  }
+};
