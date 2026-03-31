990cc40e3186b41c1ca3d4d8498c9ecf818ef2e4
Meek Meek <haggins.jameka@gmail.com>
2026-03-31 13:24:29 -0400

    I've been trying to work through this 'next is not a function' error for the last 45 minutes...and coming back after lunch made me realize...I didn't need next() to begin with. The headache isn't gone but the show must go on.

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,28 +3,27 @@ import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   username: {
-    type: String, 
+    type: String,
     required: true,
     unique: true,
     trim: true
   },
   password: {
-    type: String, 
+    type: String,
     required: true
   }
-}, { timestamps: true});
+}, { timestamps: true });
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
 
   try {
     const saltRounds = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, saltRounds);
-    next();
   } catch (err) {
-    next(err);
+    throw (err);
   }
 })
 
