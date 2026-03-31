d1c179bc1c031a05838e560ce7604b2b0f897bfe
Meek Meek <haggins.jameka@gmail.com>
2026-03-31 13:48:04 -0400

    updated the dashboard to add a logout button.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,14 +1,21 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
   const { user, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
 
   return (
     <div>
       <h1>Welcome Back, {user?.username}!</h1>
       <p>Your active Campaigns will appear here.</p>
-      <button onClick={logout}>Leave the Realm (Logout)</button>
+      <button onClick={handleLogout}>Leave the Realm</button>
     </div>
   );
 }
