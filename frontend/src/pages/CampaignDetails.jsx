693253b21e80bc54ccdfe8f70201abd53734e2ae
Meek Meek <haggins.jameka@gmail.com>
2026-04-01 09:44:30 -0400

    updating the CampaignDetails again, this time we're adding in the handshake with the backend for quests...aka tasks.

--- frontend/src/pages/CampaignDetails.jsx
+++ frontend/src/pages/CampaignDetails.jsx
@@ -6,24 +6,47 @@ function CampaignDetails() {
   const { id } = useParams();
   const navigate = useNavigate();
   const [campaign, setCampaign] = useState(null);
+  const [quests, setQuests] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [newQuest, setNewQuest] = useState({ name: "", description: ""});
 
 
   useEffect(() => {
-   async function fetchDetails() {
-    try {
-      const response = await api.get(`/campaigns/${id}`, {
-        headers: { Authorization: `Bearer ${localStorage.getItem("token")}`}
-      });
-      setCampaign(response.data);
-    } catch (err) {
-      console.error("Could not find this adventure:", err);
-    } finally {
-      setLoading(false);
+    async function fetchData() {
+      try {
+        const campRes = await api.get(`/campaigns/${id}`, {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}`}
+        });
+        setCampaign(campRes.data);
+
+        const questRes = await api.get(`/quests/${id}`, {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}`}
+        });
+        setQuests(questRes.data);
+      } catch (err) {
+        console.error("Fetch error:", err);
+      } finally {
+        setLoading(false);
+      }
     }
-   }
-   fetchDetails();
+    fetchData();
   }, [id]);
+
+  // useEffect(() => {
+  //  async function fetchDetails() {
+  //   try {
+  //     const response = await api.get(`/campaigns/${id}`, {
+  //       headers: { Authorization: `Bearer ${localStorage.getItem("token")}`}
+  //     });
+  //     setCampaign(response.data);
+  //   } catch (err) {
+  //     console.error("Could not find this adventure:", err);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  //  }
+  //  fetchDetails();
+  // }, [id]);
 
   if (loading) return <p>Consulting the map...</p>;
   if (!campaign) return <p>Campaign not found.</p>;
